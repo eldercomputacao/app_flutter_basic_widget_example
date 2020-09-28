d26aa75a03d43f24f7c3f23d6da30e2cbec28e27
Elder <elder.computacao@gmail.com>
2020-09-28 15:38:42 -0300

    setimo commit

---
+++ lib/aula_12_http_consumo_api/listview_posts_widget.dart
@@ -0,0 +1,51 @@
+import 'package:app_flutter_basic_widget_example/aula_12_http_consumo_api/post.dart';
+import 'package:app_flutter_basic_widget_example/aula_12_http_consumo_api/post_api.dart';
+import 'package:flutter/material.dart';
+
+class ListViewPostWidget extends StatefulWidget {
+  @override
+  _ListViewPostWidgetState createState() => _ListViewPostWidgetState();
+}
+
+class _ListViewPostWidgetState extends State<ListViewPostWidget> {
+  allPost() async {
+    await PostApi.allPost();
+  }
+
+  getPostId() async {
+    await PostApi.getPostId(1);
+  }
+
+  allComments() async {
+    await PostApi.allCommentsPost(1);
+  }
+
+  savePost() async {
+    Post post = Post(userId: 10, title: 'title', body: 'body');
+    await PostApi.savePost(post);
+  }
+
+  deletePost() async {
+    await PostApi.deletePost(-2).then((value) => print(value));
+  }
+
+  putPost() async {
+    Post post = Post(id: 1, userId: 10, title: 'title', body: 'body');
+    await PostApi.putPost(post);
+  }
+
+  patchPost() async {
+    Post post = Post(id: 1, userId: 5, title: 'title', body: 'body');
+    await PostApi.patchPost(post);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    patchPost();
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:app_flutter_basic_widget_example/aula_06_listView/exemplos_listview_widget.dart';
-import 'package:app_flutter_basic_widget_example/aula_10_image/exemplo_image_picker_foto.dart';
+import 'package:app_flutter_basic_widget_example/aula_10_image/exemplo_imagepicker_camera_gallery.dart';
 import 'package:app_flutter_basic_widget_example/aula_10_image/exemplo_image_picker_galeria.dart';
 import 'package:app_flutter_basic_widget_example/aula_10_image/exemplo_image_widget.dart';
 import 'package:app_flutter_basic_widget_example/aula_11_gridview/exemplo_gridview_widget.dart';
+import 'package:app_flutter_basic_widget_example/aula_12_http_consumo_api/listview_posts_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,10 @@ void main() {
       //home: ExemplosListWidget(),
       //home: ListaCarroWidget(),
       //home: ExemploImageWidget(),
-      home: ExemploImagePickerGaleriaWidget(),
+      //home: ExemploImagePickerCameraGalleryWidget(),
       //home: ExemploGridViewWidget(),
       //home: CategoriaTelefonesWidget(),
+      home: ListViewPostWidget(),
     ),
   );
 }
